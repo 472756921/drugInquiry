0ef1bec875e8de47697bb3e87266c0a900e867c4
BensonChen <472756921@qq.com>
2019-03-27 18:08:27 +0800

    爬虫优化

---
+++ app/schedule/pull_refresh.js
@@ -0,0 +1,16 @@
+module.exports = {
+    schedule: {
+        interval: '8h', // 1 分钟间隔
+        type: 'all', // 指定所有的 worker 都需要执行
+    },
+    async task(ctx) {
+        const {data, code} = await ctx.service.spider.Spider();
+        let firstData = await ctx.service.drug.FdaFirst();
+        firstData = firstData.results;
+        const tempData = data[0];
+        if((firstData.length==0 ? 0 : firstData[0].tempDateFromFDA) !== tempData.USdate){
+            ctx.app.messenger.sendToApp('insert_batch', { data: data, date: firstData.length==0 ? 0 : firstData[0].tempDateFromFDA });
+            ctx.logger.info('auto schedule to get FDA data');
+        }
+    },
+};
